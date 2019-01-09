2832ca5add59698421c85e229dcc0cae6852ebfa
CLacaile <clementlacaile@gmail.com>
2019-01-09 15:40:43 +0100

    added test for createCompteur3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import DAO.CompteurDAO;
+import MODELE.Adresse;
+import MODELE.Personne;
+import net.bytebuddy.asm.Advice;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.time.LocalDate;
 
 public class Main {
 
@@ -8,11 +14,18 @@ public class Main {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("bdd_hibernate");
         EntityManager em = emf.createEntityManager();
 
-        em.getTransaction().begin();
 
+        //Test de création compteur 3 avec une personne existante
+        Adresse adr1 = new Adresse();
+        adr1.setRue("Parmentier");
+        adr1.setVille("Tours");
+        Personne p1 = new Personne();
+        p1.setNumSS(11111);
+        p1.setNumTel("0606060606");
+        p1.setAdresse(adr1);
+        adr1.setPersonne(p1);
+        LocalDate date1 = LocalDate.now();
+        CompteurDAO.createCompteur(em, date1, p1);
 
-
-
-        em.getTransaction().commit();
     }
 }
