d68f3fb612789016f318e5d5bbaed3599651d690
CLacaile <clementlacaile@gmail.com>
2019-01-09 11:52:33 +0100

    deleted comment

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,12 @@ import javax.persistence.Persistence;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// Etant donné un tarif HC, on veut vérifier s'il a été appliqué à au moins une plage de consommation
-		// Etant donné un compteur et une date, on veut calculer le cout total des consommations effectuées
-		// le jour donné (avec application éventuelle des tarifs HC)
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("bdd_hibernate");
-		EntityManager em = emf.createEntityManager();
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("bdd_hibernate");
+        EntityManager em = emf.createEntityManager();
 
-		em.getTransaction().begin();
+        em.getTransaction().begin();
 
-		em.getTransaction().commit();
-
-	}
-
+        em.getTransaction().commit();
+    }
 }
