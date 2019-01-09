dd872342ada35ead5b888d9e021d38a6cf144b31
CLacaile <clementlacaile@gmail.com>
2019-01-09 16:13:48 +0100

    added test for createCompteur1 and createCompteur2 and updatePersonne()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import DAO.CompteurDAO;
 import MODELE.Adresse;
+import MODELE.Compteur;
 import MODELE.Personne;
 import net.bytebuddy.asm.Advice;
 
@@ -26,6 +27,25 @@ public class Main {
         adr1.setPersonne(p1);
         LocalDate date1 = LocalDate.now();
         CompteurDAO.createCompteur(em, date1, p1);
+
+        //Test de création compteur 1 avec une adresse existante
+        Adresse adr3 = new Adresse();
+        adr3.setRue("Parmentier");
+        adr3.setVille("Tours");
+        Personne p2 = new Personne();
+        p2.setNumSS(22222);
+        p2.setNumTel("0606060606");
+        p2.setAdresse(adr3);
+        adr3.setPersonne(p2);
+        CompteurDAO.createCompteur(em, date1, adr3);
+
+        //Test de création compteur 2 sans adresse existante ni personne
+        Compteur compt = CompteurDAO.createCompteur(em, date1, "Grammont", "Tours");
+        Personne p3 = new Personne();
+        p3.setNumSS(33333);
+        p3.setNumTel("0606060606");
+        CompteurDAO.updatePersonne(em, compt, p3);
+
 
     }
 }
