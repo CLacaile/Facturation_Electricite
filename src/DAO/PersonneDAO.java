e7c49b0f0883bf6677315284a2dd2307b3c731ba
CLacaile <clementlacaile@gmail.com>
2019-01-09 13:24:26 +0100

    created createPersonne()

--- src/DAO/PersonneDAO.java
+++ src/DAO/PersonneDAO.java
@@ -1,5 +1,25 @@
 package DAO;
 
-public class PersonneDAO {
+import MODELE.Adresse;
+import MODELE.Compteur;
+import MODELE.Personne;
 
+import javax.persistence.EntityManager;
+import java.time.LocalDate;
+
+public class PersonneDAO {
+    public static Personne createPersonne(EntityManager em, long numSS, Compteur compteur, Adresse adresse, String numTel) {
+        Personne pers = new Personne();
+        pers.setNumSS(numSS);
+        pers.setNumTel(numTel);
+        pers.setAdresse(adresse);
+        pers.setCompteur(compteur);
+        compteur.setPersonne(pers);
+
+        em.getTransaction().begin();
+        em.persist(pers);
+        em.persist(compteur);
+        em.getTransaction().commit();
+        return pers;
+    }
 }
