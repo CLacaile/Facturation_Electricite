b88cce612b0faf4906f5e0715ad5773d5304c276
CLacaile <clementlacaile@gmail.com>
2019-01-10 12:03:20 +0100

    created file + added find()

---
+++ src/DAO/TarifDAO.java
@@ -0,0 +1,18 @@
+package DAO;
+
+import MODELE.Tarif;
+
+import javax.persistence.EntityManager;
+
+public class TarifDAO {
+
+    /**
+     * Find a tarif in the DB
+     * @param em the EntityManager
+     * @param id the id
+     * @return the Tarif or null if it doesn't exist
+     */
+    public static Tarif find(EntityManager em, long id) {
+        return (Tarif) em.find(Tarif.class, id);
+    }
+}
