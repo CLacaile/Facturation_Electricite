61c4f6962620aafce9caf31bca6ff7c34bd5d267
CLacaile <clementlacaile@gmail.com>
2019-01-11 14:19:25 +0100

    added createTarifCreux()

--- src/DAO/TarifCreuxDAO.java
+++ src/DAO/TarifCreuxDAO.java
@@ -3,29 +3,34 @@ package DAO;
 import MODELE.Horaires;
 import MODELE.Tarif;
 import MODELE.TarifCreux;
+import net.bytebuddy.asm.Advice;
 
 import javax.persistence.EntityManager;
+import java.time.LocalTime;
 
-public class TarifCreuxDAO extends TarifDAO {
+public class TarifCreuxDAO {
+
+    /**
+     * Create a tarif creux in the db
+     * @param em
+     * @param prix
+     * @param heureDeb
+     * @param heureFin
+     * @return
+     */
+    protected static TarifCreux createTarifCreux(EntityManager em, double prix, LocalTime heureDeb, LocalTime heureFin) {
+        TarifCreux tarif = new TarifCreux();
+        tarif.setPrix(prix);
+        tarif.setHeureDeb(heureDeb);
+        tarif.setHeureFin(heureFin);
+        em.getTransaction().begin();
+        em.persist(tarif);
+        em.getTransaction().commit();
+        return tarif;
+    }
 
     public static TarifCreux updateHoraires(EntityManager em, TarifCreux tarifToUpdate, Horaires horairesToUpdate) {
-        Horaires previousHoraires = tarifToUpdate.getHoraires();
-        if(previousHoraires == null) {
-            // if the tarif has just been created
-            tarifToUpdate.setHoraires(horairesToUpdate);
-            horairesToUpdate.getTarifs().add(tarifToUpdate);
-        }
-        else if(previousHoraires != horairesToUpdate) {
-            // if the tarif already exists in an other horaires
-            previousHoraires.getTarifs().remove(tarifToUpdate);
-            tarifToUpdate.setHoraires(horairesToUpdate);
-            horairesToUpdate.getTarifs().add(tarifToUpdate);
-        }
-        em.getTransaction().begin();
-        em.persist(tarifToUpdate);
-        em.persist(horairesToUpdate);
-        em.persist(previousHoraires);
-        em.getTransaction().commit();
+
         return tarifToUpdate;
     }
 }
