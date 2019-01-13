384e13d0f82db09839a4fef856454b4a6fc2dc55
CLacaile <clementlacaile@gmail.com>
2019-01-13 19:59:13 +0100

    added scan + display functions

--- src/VUE/Vue.java
+++ src/VUE/Vue.java
@@ -1,31 +1,60 @@
 package VUE;
 
-import CONTROLEUR.Controleur;
+import MODELE.Modele;
+
+import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Vue {
-    private Controleur controleur;
+    private Modele modele;
 
-    Vue(Controleur ctrlr) {
-        this.controleur = ctrlr;
+    public Vue(Modele m) {
+        this.modele = m;
+        System.out.println("========= Facturation Electricite =========");
+        int choix = displayMenu();
     }
 
-    public void displayTarif() {
-        System.out.println(controleur.getTarif().toString());
+    public int displayMenu() {
+        System.out.println("Entrez votre choix: ");
+        System.out.println("1. Verifier a quelle consommation a ete applique un tarif creux");
+        System.out.println("2. Calculer le cout des consommations d'un compteur a une date");
+        int choix = scanInteger();
+        while (choix > 2 && choix < 1) {
+            choix = displayMenu();
+        }
+        return choix;
     }
 
-    public void displayTarifCreux() {
-        System.out.println(controleur.getTarifCreux().toString());
+    public void display(String s) {
+        System.out.println(s);
     }
 
-    public void displayTarifPlein() {
-        System.out.println(controleur.getTarifPlein().toString());
+    public int scanInteger() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int res = 0;
+        try {
+            res = Integer.valueOf(str);
+        } catch(NumberFormatException e) {
+            System.out.println("Erreur : veuillez entrer un nombre entier");
+        }
+        sc.close();
+        return res;
     }
 
-    public void displayConsommation() {
-        System.out.println(controleur.getConsommation().toString());
+    public double scanDouble() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        double res = Double.parseDouble(str);
+        sc.close();
+        return res;
     }
 
-    public void displayCompteur() {
-        System.out.println(controleur.getCompteur().toString());
+    public LocalDate scanDate() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        LocalDate date = LocalDate.parse(str);
+        sc.close();
+        return date;
     }
 }
