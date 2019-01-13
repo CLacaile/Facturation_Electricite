c72206d5f5ab2cf6c1c1680c9598808aa07b568b
CLacaile <clementlacaile@gmail.com>
2019-01-13 19:57:55 +0100

    deleted all attributes but vue and added attribute modele

--- src/CONTROLEUR/Controleur.java
+++ src/CONTROLEUR/Controleur.java
@@ -1,54 +1,15 @@
 package CONTROLEUR;
 
-import MODELE.*;
+import MODELE.Modele;
 import VUE.Vue;
 
 public class Controleur {
-    private Compteur compteur;
-    private Consommation consommation;
-    private Tarif tarif;
-    private TarifCreux tarifCreux;
-    private TarifPlein tarifPlein;
+    private Modele modele;
     private Vue vue;
 
-    public Compteur getCompteur() {
-        return compteur;
-    }
-
-    public void setCompteur(Compteur compteur) {
-        this.compteur = compteur;
-    }
-
-    public Consommation getConsommation() {
-        return consommation;
-    }
-
-    public void setConsommation(Consommation consommation) {
-        this.consommation = consommation;
-    }
-
-    public Tarif getTarif() {
-        return tarif;
-    }
-
-    public void setTarif(Tarif tarif) {
-        this.tarif = tarif;
-    }
-
-    public TarifCreux getTarifCreux() {
-        return tarifCreux;
-    }
-
-    public void setTarifCreux(TarifCreux tarifCreux) {
-        this.tarifCreux = tarifCreux;
-    }
-
-    public TarifPlein getTarifPlein() {
-        return tarifPlein;
-    }
-
-    public void setTarifPlein(TarifPlein tarifPlein) {
-        this.tarifPlein = tarifPlein;
+    public Controleur(Modele m, Vue v) {
+        this.modele = m;
+        this.vue = v;
     }
 
     public Vue getVue() {
