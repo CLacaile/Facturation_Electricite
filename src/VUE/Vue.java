b436a59b99cb7e8aa3aca266fbb474c8bddb0a21
CLacaile <clementlacaile@gmail.com>
2019-01-13 17:13:40 +0100

    added display()

--- src/VUE/Vue.java
+++ src/VUE/Vue.java
@@ -1,4 +1,31 @@
 package VUE;
 
+import CONTROLEUR.Controleur;
+
 public class Vue {
+    private Controleur controleur;
+
+    Vue(Controleur ctrlr) {
+        this.controleur = ctrlr;
+    }
+
+    public void displayTarif() {
+        System.out.println(controleur.getTarif().toString());
+    }
+
+    public void displayTarifCreux() {
+        System.out.println(controleur.getTarifCreux().toString());
+    }
+
+    public void displayTarifPlein() {
+        System.out.println(controleur.getTarifPlein().toString());
+    }
+
+    public void displayConsommation() {
+        System.out.println(controleur.getConsommation().toString());
+    }
+
+    public void displayCompteur() {
+        System.out.println(controleur.getCompteur().toString());
+    }
 }
